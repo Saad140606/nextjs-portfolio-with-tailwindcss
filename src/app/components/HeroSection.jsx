a09858cc5867503b5c8d31dfebe95e43326d2e55
Saad140606 <syedsaadnajam2006@gmail.com>
2024-12-16 12:06:31 +0500

    first commit

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,70 +1,38 @@
-"use client";
-import React from "react";
-import Image from "next/image";
-import { TypeAnimation } from "react-type-animation";
-import { motion } from "framer-motion";
-import Link from "next/link";
-
-const HeroSection = () => {
+export default function Home() {
   return (
-    <section className="lg:py-16">
-      <div className="grid grid-cols-1 sm:grid-cols-12">
-        <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
-        >
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-              Hello, I&apos;m{" "}
-            </span>
-            <br></br>
-            <TypeAnimation
-              sequence={[
-                "Saad",
-                1000,
-                "Web Developer",
-                1000,
-                "Front-End Developer",
-                1000,
-                "UI/UX Designer",
-                1000,
-              ]}
-              wrapper="span"
-              speed={50}
-              repeat={Infinity}
-            />
+    <main>
+       
+      {/* Upper Navbar */}
+      <div className=" h-14 bg-gray-300"></div>
+           <div className="min-h-screen flex items-center justify-center bg-D9D9D9 ">
+      <div className="max-w-6xl mx-auto bg-white  flex items-center p-8 rounded-md">
+        {/* Left Section */}
+        <div className="flex-1">
+          <h1 className="text-4xl font-bold mb-4 text-black">
+            Welcome To Our Website
           </h1>
-          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-          I am a Front-End Developer and UI/UX Designer building and designing websites and web apps from scratch.
-          Html, CSS, Bootstrap, Javascript, Typescript, React.js, Next.js, and Python are the field of my expertise when it comes to building websites and web apps. I deal with servers as well.
-          I am flawless using Figma, Adobe XD, Illustrator, and Photoshop for the design. Photography is also one of the skills I use in my design.
-          </p>
-          <div>
-     
-          </div>
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-          className="col-span-4 place-self-center mt-4 lg:mt-0"
-        >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-            <Image
-              src="/images/hero-image.png"
-              alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 "
-              width={400}
-              height={600}
-              border={100}
+          <p className="text-gray-500 w-571 h-156 text-2xl align-center">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. <br /> Lorem Ipsum has been the industry's <br /> standard.
+          </p><br /><br />
+          <button className="bg-black text-white py-2 px-4 w-237 h-66 hover:bg-gray-800">
+            Contact US
+          </button>
+        </div>
+
+        {/* Right Section */}
+        <div className="flex-1 flex justify-center">
+          <div className="w-502 h-465  overflow-hidden ">
+            <img
+              src="/image/image.png"
+              alt="Abstract Black"
+              className="w-full h-full object-cover"
             />
           </div>
-        </motion.div>
+        </div>
       </div>
-    </section>
+    </div>
+    </main>
+    
   );
-};
-
-export default HeroSection;
+}
